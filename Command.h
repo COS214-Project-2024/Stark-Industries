5bcfad62efecd982b6fd33e867ae200601990214
tishreddy <u19072211@tuks.co.za>
2024-10-24 15:36:01 +0200

    formatting

--- Command.h
+++ Command.h
@@ -1,14 +1,20 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
+#include "Department.h"
+
 class Command {
 
-
-//note
-
-
 public:
+	Department * receiver; 
 	void execute();
 };
 
 #endif
+
+/*
+Command Command Pattern:
+	Invoker: Department
+	ConcreteCommand: Allocatetax, CollectTax, IncreaseTax
+	Receiver: Department
+*/
