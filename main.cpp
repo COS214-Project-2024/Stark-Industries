d21ed0ffd77142dd8e7f60349a40b26530dbb697
bahiya6 <u22598546@tuks.co.za>
2024-10-24 12:26:01 +0200

    testing for factory utilites done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include "WaterFactory.h"
+#include "WasteFactory.h"
+#include "SewageFactory.h"
+#include "PowerPlantFactory.h"
+#include "Utilities.h"
+#include "Water.h"
+#include "Waste.h"
+#include "Sewage.h"
+
+void testFactoryUtilities() {
+    // Water Factory
+    WaterFactory waterFactory;
+    Utilities* waterUtility = waterFactory.createUtility();
+    cout << "Testing Water Utility:\n";
+    waterUtility->displayInfo();
+    waterUtility->generateResources();
+    waterUtility->requestMaintenance(101);
+    cout << endl;
+
+    // Waste Factory
+    WasteFactory wasteFactory;
+    Utilities* wasteUtility = wasteFactory.createUtility();
+    cout << "Testing Waste Utility:\n";
+    wasteUtility->displayInfo();
+    wasteUtility->generateResources();
+    wasteUtility->requestMaintenance(102);
+    cout << endl;
+
+    // Sewage Factory
+    SewageFactory sewageFactory;
+    Utilities* sewageUtility = sewageFactory.createUtility();
+    cout << "Testing Sewage Utility:\n";
+    sewageUtility->displayInfo();
+    sewageUtility->generateResources();
+    sewageUtility->requestMaintenance(103);
+    cout << endl;
+
+    // PowerPlant Factory
+    PowerPlantFactory powerPlantFactory;
+    Utilities* powerPlantUtility = powerPlantFactory.createUtility();
+    cout << "Testing Power Plant Utility:\n";
+    powerPlantUtility->displayInfo();
+    powerPlantUtility->generateResources();
+    powerPlantUtility->requestMaintenance(104);
+    cout << endl;
+
+    // Clean up
+    delete waterUtility;
+    delete wasteUtility;
+    delete sewageUtility;
+    delete powerPlantUtility;
+}
 
 int main() {
-    std::cout << "Helloooooo ;)" << std::endl;
+    // Only the test function call
+    testFactoryUtilities();
     return 0;
 }
