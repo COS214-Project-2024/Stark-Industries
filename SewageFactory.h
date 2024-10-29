437b4e9e3c5d946c9d32341100a93a0d2141bc65
bahiya6 <u22598546@tuks.co.za>
2024-10-29 21:36:55 +0200

    doxygen

--- SewageFactory.h
+++ SewageFactory.h
@@ -6,11 +6,26 @@
 #include <string>
 using namespace std;
 
+/**
+ * @brief Concrete factory class for creating sewage utility objects.
+ * 
+ * The SewageFactory class is a concrete implementation of the 
+ * UtilitiesFactory, providing functionality to create specific 
+ * types of utilities related to sewage management.
+ */
 class SewageFactory : public UtilitiesFactory {
 
-
 public:
-	Utilities* createUtility();
+    /**
+     * @brief Creates a sewage utility object.
+     * 
+     * This method overrides the createUtility method in the 
+     * UtilitiesFactory class and returns a pointer to a newly 
+     * created Utilities object specific to sewage management.
+     * 
+     * @return A pointer to the newly created Utilities object.
+     */
+    Utilities* createUtility();
 };
 
 #endif
