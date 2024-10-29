b692bd154b296aa51693390eb52a71322f95f05c
bahiya6 <u22598546@tuks.co.za>
2024-10-29 21:37:11 +0200

    doxygen

--- Utilities.h
+++ Utilities.h
@@ -4,28 +4,69 @@
 #include <string>
 using namespace std;
 
+/**
+ * @brief Abstract base class for utilities.
+ * 
+ * The Utilities class provides a common interface for all utility 
+ * types, including methods for generating resources, displaying 
+ * information, and requesting maintenance. It is intended to be 
+ * subclassed by specific utility implementations.
+ */
 class Utilities {
 
 private:
-	string name;
-	double capacity;
-	double operationalCost;
+    string name;             ///< The name of the utility.
+    double capacity;         ///< The maximum capacity of the utility.
+    double operationalCost;  ///< The operational cost associated with the utility.
 
 public:
-
-	// Constructor for initializing utility details
+    /**
+     * @brief Constructs a Utilities object with specified details.
+     * 
+     * This constructor initializes a Utilities object with the given 
+     * name, capacity, and operational cost.
+     * 
+     * @param name The name of the utility.
+     * @param capacity The maximum capacity of the utility.
+     * @param operationalCost The operational cost of the utility.
+     */
     Utilities(string name, double capacity, double operationalCost)
         : name(name), capacity(capacity), operationalCost(operationalCost) {}
 
+    /**
+     * @brief Generates resources from the utility.
+     * 
+     * This is a pure virtual function that must be overridden by 
+     * derived classes to implement specific resource generation logic.
+     */
+    virtual void generateResources() = 0;
 
-	virtual void generateResources() = 0;
+    /**
+     * @brief Displays information about the utility.
+     * 
+     * This is a pure virtual function that must be implemented by 
+     * derived classes to output details about the utility.
+     */
+    virtual void displayInfo() = 0;
 
-	virtual void displayInfo() = 0;
+    /**
+     * @brief Requests maintenance for the utility.
+     * 
+     * This is a pure virtual function that must be implemented by 
+     * derived classes to handle maintenance requests.
+     * 
+     * @param Utilities_util An integer identifier for the type of utility 
+     *                       requesting maintenance.
+     */
+    virtual void requestMaintenance(int Utilities_util) = 0;
 
-	virtual void requestMaintenance(int Utilities_util) = 0;
-
-	virtual ~Utilities() = default;  // Virtual destructor for cleanup
-
+    /**
+     * @brief Virtual destructor for the Utilities class.
+     * 
+     * This ensures that derived class destructors are called correctly 
+     * when an object is deleted through a pointer to the base class.
+     */
+    virtual ~Utilities() = default;  // Virtual destructor for cleanup
 };
 
 #endif
--- UtilitiesFactory.h
+++ UtilitiesFactory.h
@@ -3,13 +3,29 @@
 
 #include "Utilities.h"
 
+/**
+ * @brief Abstract base class for creating utility objects.
+ * 
+ * The UtilitiesFactory class defines an interface for creating utility 
+ * objects. Concrete factories will implement the `createUtility` method 
+ * to instantiate specific types of utilities.
+ */
 class UtilitiesFactory {
 
 private:
-	Utilities* utilities;
+    Utilities* utilities; ///< Pointer to a Utilities object for internal use by derived classes.
 
 protected:
-	virtual Utilities* createUtility() = 0;
+    /**
+     * @brief Creates a utility object.
+     * 
+     * This is a pure virtual function that must be overridden in derived 
+     * classes to provide specific functionality for creating instances 
+     * of Utilities.
+     * 
+     * @return A pointer to the newly created Utilities object.
+     */
+    virtual Utilities* createUtility() = 0; // Pure virtual function
 };
 
 #endif
