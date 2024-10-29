4eeb0efd7182f44f87c9b4dcbad96f4c5ed6d3ff
Thuwayba15 <u22622668@tuks.co.za>
2024-10-29 17:26:04 +0200

    Subject: Building

--- Building.cpp
+++ Building.cpp
@@ -1,4 +1,8 @@
 #include "Building.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 
 Building::Building(string name, int satisfaction, double economicImpact, double resourceConsumption, bool constructionStatus, int improvementLevel, bool resourcesAvailable, int notificationRadius) : name(name),
       satisfaction(satisfaction),
@@ -17,4 +21,22 @@ void Building::get() {
 void Building::set() {
 	// TODO - implement Building::set
 	throw "Not yet implemented";
+}
+
+// Attach a citizen to the observer list
+void Building::attach(Citizen* observer) {
+    observerList.push_back(observer);
+}
+
+// Detach a citizen from the observer list
+void Building::detach(Citizen* observer) {
+    observerList.erase(std::remove(observerList.begin(), observerList.end(), observer), observerList.end());
+}
+
+// Notify all observers (citizens) about changes
+void Building::notifyCitizens() {
+    std::cout << "Notifying citizens about changes in " << name << "...\n";
+    for (Citizen* citizen : observerList) {
+        citizen->observerUpdate();  // Call the observer's update method
+    }
 }
